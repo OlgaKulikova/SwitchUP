2dc06c1c929476235a321c39b41971dd2e554278
savaroma <savaroma@gmail.com>
2016-03-27 14:40:10 +0300

    start

--- src/main/java/org/rest/quest/RestClientApplication.java
+++ src/main/java/org/rest/quest/RestClientApplication.java
@@ -20,18 +20,18 @@ public class RestClientApplication {
 
         //registrationHelper
         RegistrationHelper registrationHelper = new RegistrationHelper();
-        registrationHelper.registerTeam(HOST_URL, "automat", null);
+        registrationHelper.registerTeam(HOST_URL, "SwitchUP", "Everyone can Switch");
 
-        //1st wall
-        String keyword = new MottoHelper().getKeyword(HOST_URL, null, <your team id>, 0, 221);
-        //2st wall
-        int secondPuzzle = new PuzzleHelper().readPuzzleHeader(HOST_URL, keyword, <your team id>,  0);
-        keyword = PuzzleService.getPuzzle(4);
-        //3rd wall
-        new UserResolver().readUser(HOST_URL, "keyword", 0);
-        //4st wall
-        WinnerResolver.sendWinnerUrl(HOST_URL, keyword, teamId, false);
-        logger.info("we are winners");
+//        //1st wall
+//        String keyword = new MottoHelper().getKeyword(HOST_URL, null, <your team id>, 0, 221);
+//        //2st wall
+//        int secondPuzzle = new PuzzleHelper().readPuzzleHeader(HOST_URL, keyword, <your team id>,  0);
+//        keyword = PuzzleService.getPuzzle(4);
+//        //3rd wall
+//        new UserResolver().readUser(HOST_URL, "keyword", 0);
+//        //4st wall
+//        WinnerResolver.sendWinnerUrl(HOST_URL, keyword, teamId, false);
+//        logger.info("we are winners");
     }
 
 }
